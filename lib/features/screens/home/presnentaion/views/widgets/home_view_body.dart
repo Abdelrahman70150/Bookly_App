d10f1645c7238b32971653d2d58858b4b664592f
Abdelrahman70150 <abdomahmoud.191198@gmail.com>
2024-08-29 09:32:33 +0200

    add test image & add list view t

--- lib/features/screens/home/presnentaion/views/widgets/home_view_body.dart
+++ lib/features/screens/home/presnentaion/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-
+import 'package:clean_arch/core/utils/assets.dart';
 import 'package:clean_arch/features/screens/home/presnentaion/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -8,15 +8,48 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: EdgeInsets.only(left: 30.w),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SizedBox(height: 48.h,),
+          SizedBox(
+            height: 48.h,
+          ),
           const CustomAppBar(),
+          const SizedBox(
+            height: 47,
+          ),
+          SizedBox(
+              height: 224.h,
+              child: ListView.separated(
+                itemBuilder: (context, index) => const CustomListViewItem(),
+                itemCount: 5,
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+                separatorBuilder: (context,index)=>SizedBox(width: 15.w,),
+              ))
         ],
       ),
     );
   }
 }
 
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150.w,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            AssetsData.testImage,
+          ),
+        ),
+      ),
+    );
+  }
+}
