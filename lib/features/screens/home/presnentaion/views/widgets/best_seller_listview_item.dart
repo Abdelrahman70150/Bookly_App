53e3f1264cf9c49cd2906cfa4189b377f8989925
Abdelrahman70150 <abdomahmoud.191198@gmail.com>
2024-10-03 08:32:28 +0200

    create book details view screen

--- lib/features/screens/home/presnentaion/views/widgets/best_seller_listview_item.dart
+++ lib/features/screens/home/presnentaion/views/widgets/best_seller_listview_item.dart
@@ -9,47 +9,42 @@ class BestSellerListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: SizedBox(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(AssetsData.testImage,height: 105.h,),
-            SizedBox(width: 30.w,),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return SizedBox(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(AssetsData.testImage,height: 105.h,),
+          SizedBox(width: 30.w,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Harry Potter\nand the Goblet of Fire',
+                style: Styles.textStyle19,
+                maxLines: 2,
+              ),
+              const Text('J.K. Rowling',
+                style: Styles.textStyle14,
+              ),
+              Row(
                 children: [
-                  Text('Harry Potter\nand the Goblet of Fire',
+                  Text('19.99 €',
                     style: Styles.textStyle19,
-                    maxLines: 2,
                   ),
-                  const Text('J.K. Rowling',
+                  SizedBox(width: 36.3.w,),
+                  const Icon(Icons.star_rounded,color: starColor),
+                  SizedBox(width: 6.3.w,),
+                  const Text('4.8',
+                    style: Styles.textStyle16,
+                  ),
+                  SizedBox(width: 5.w,),
+                  const Text('(2390)',
                     style: Styles.textStyle14,
                   ),
-                  Row(
-                    children: [
-                      Text('19.99 €',
-                        style: Styles.textStyle19,
-                      ),
-                      SizedBox(width: 36.3.w,),
-                      const Icon(Icons.star_rounded,color: starColor),
-                      SizedBox(width: 6.3.w,),
-
-                      const Text('4.8',
-                        style: Styles.textStyle16,
-                      ),
-                      SizedBox(width: 5.w,),
-                      const Text('(2390)',
-                        style: Styles.textStyle14,
-                      ),
-                    ],
-                  )
                 ],
-              ),
-            )
-          ],
-        ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
