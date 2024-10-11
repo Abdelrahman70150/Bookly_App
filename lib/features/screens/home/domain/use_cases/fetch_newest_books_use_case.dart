28e6657039f5af04abe50feb5f9227ee035804d9
Abdelrahman70150 <abdomahmoud.191198@gmail.com>
2024-10-11 09:41:03 +0200

    Add Fetch Newest Books Use Case

--- lib/features/screens/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/screens/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -4,17 +4,6 @@ import 'package:Bookly/features/screens/home/domain/repos/home_repo.dart';
 import 'package:Bookly/features/screens/home/domain/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
-  final HomeRepo homeRepo;
-
-  FetchFeaturedBooksUseCase(this.homeRepo);
-
-  @override
-  Future<Either<Failure, List<BookEntity>>> call({NoParam? param}) async {
-    return await homeRepo.fetchFeaturedBook();
-  }
-}
-
 class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepo homeRepo;
 
@@ -22,7 +11,6 @@ class FetchNewestBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
 
   @override
   Future<Either<Failure, List<BookEntity>>> call({NoParam? param}) async {
-    return await homeRepo.fetchFeaturedBook();
+    return await homeRepo.fetchNewestBook();
   }
 }
-
