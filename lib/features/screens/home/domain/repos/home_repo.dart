7fcf899070f29cd9a972cc2390796ba94ea51144
Abdelrahman70150 <abdomahmoud.191198@gmail.com>
2024-10-11 08:58:24 +0200

    add dartz package & failure class

--- lib/features/screens/home/domain/repos/home_repo.dart
+++ lib/features/screens/home/domain/repos/home_repo.dart
@@ -1,6 +1,8 @@
+import 'package:Bookly/core/errors/failure.dart';
 import 'package:Bookly/features/screens/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
-  Future<List<BookEntity>> fetchFeaturedBook();
-  Future<List<BookEntity>> fetchNewestBook();
+  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBook();
+  Future<Either<Failure,List<BookEntity>>> fetchNewestBook();
 }
